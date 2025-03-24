8b5cc1646f9db2bd2a12ce1b4e7741c1e25bfa0b
Phylip28 <felipe28769@gmail.com>
2025-03-24 12:50:41 -0500

    correct data joining

--- join_dataframes.py
+++ join_dataframes.py
@@ -1,33 +1,60 @@
 import pandas as pd
 import os
+from collections import Counter
 
-# path for files by normal activity
-dataset_path = "data/Training_Data_Master"
+try:
+    # path for files by normal activity
+    dataset_path = "data/Training_Data_Master"
 
-# list of files
-files = [f for f in os.listdir(dataset_path) if f.endswith(".txt")]
+    # list of files
+    files = [
+        os.path.join(dataset_path, f)
+        for f in os.listdir(dataset_path)
+        if f.endswith(".txt")
+    ]
 
-# list of dataframes
-dfs = []
+# verify if the contain files
+except FileNotFoundError:
+    print("No files found in the dataset path")
+    exit()
+else:
+    print(f"Found {len(files)} files")
 
-# read all archives and append to dfs
-for file in files:
-    full_path = os.path.join(dataset_path, file)
+    # global counter of syscalls
+    syscalls_counter = Counter()
+    total_syscalls = 0
 
-    # read file before to process
-    with open(full_path, "r") as f:
-        sample = f.readline().strip()
-        print(f"File: {file} -> {sample}")  # show the content
+    # read and process each file
+    for file in files:
 
-    # read the file as dataframe
-    df = pd.read_csv(full_path, sep=r"\s+", header=None, names=["syscall_id"])
-    dfs.append(df)
+        with open(file, "r") as f:
+            line = f.readline().strip()  # remove spaces
+            converted_to_numbers = map(int, line.split())  # convert to integers type
+            syscall_list = list(converted_to_numbers)  # save to list
+            total_syscalls += len(syscall_list)
+            syscalls_counter.update(syscall_list)
 
-# join all dataframes
-df_combined = pd.concat(dfs, ignore_index=True)
+# create a dataframe with the results
+df = pd.DataFrame(syscalls_counter.items(), columns=["syscall_id", "frequency"])
+df = df.sort_values(by="frequency", ascending=False).reset_index(drop=True)
 
-# show first lines
-print(df_combined.head())
+# check if the sum of frequencies match with the total datasets read
+sum_frequencies = df["frequency"].sum()
 
-# save the dataframe
-df_combined.to_csv("data/Training_Data_Master/joined_data.csv", index=False)
+print(f"Total syscalls read: {total_syscalls}")
+print(f"Sum of frequencies: {sum_frequencies}")
+
+if total_syscalls == sum_frequencies:
+    print("All its alright")
+else:
+    print("It's have a problem")
+    exit()
+
+# print fist rows of the dataframe
+print(df.head())
+
+# save the dataframe to a csv file
+try:
+    df.to_csv("data/Training_Data_Master/dataset.csv", index=False)
+except Exception as e:
+    print(f"error saving the file: {e}")
